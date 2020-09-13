d64735ec02a82940d08a428851bca9563abe9af6
shamdy <shamdy@bu.edu>
2020-09-13 00:26:08 -0400

    added survey
    no symptom checking YET (doesn't check if answered yes on any symptoms)

--- app/src/main/java/com/example/bucovidmonitor/app/HomeScreen.java
+++ app/src/main/java/com/example/bucovidmonitor/app/HomeScreen.java
@@ -6,13 +6,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bucovidmonitor.R;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 public class HomeScreen extends AppCompatActivity {
 
+    // private Button surveyBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        final Button surveyBtn = findViewById(R.id.button2);
+        surveyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), SurveyActivity.class));
+            }
+        });
 
     }
 }
